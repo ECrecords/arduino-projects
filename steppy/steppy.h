fb9601435b9d68558622f95f04236b299b0d3972
Elvis Chino-Islas <echinoislas+github@gmail.com>
2023-01-28 15:16:13 -0800

    elvis: parital completed switch state machine

--- steppy/steppy.h
+++ steppy/steppy.h
@@ -2,18 +2,41 @@
  * @ Author: Elvis Chino-Islas
  * @ Create Time: 2023-01-27 21:26:50
  * @ Modified by: Elvis Chino-Islas
- * @ Modified time: 2023-01-27 21:36:56
+ * @ Modified time: 2023-01-28 14:51:10
  * @ Description:
  */
+#pragma once
 
 #include <pins_arduino.h>
+#include "a4988.h"
 
-#define INPUT1  PIN2
-#define INPUT2  PIN3
+#define BAUD_RATE   115200
 
+#define INPUT1      PIN2
+#define INPUT2      PIN3
 
+#define STEP        PIN5 
+#define ENABLE      PIN4
+#define DIR         PIN6
 enum STEP_STATES_T
 {
-    STOPPED,
-    RUNNING
+    STOP_INIT,
+    RUN_INIT,
+
+    RUN_SPEED1,
+    STOP_SPEED1,
+    
+    RUN_SPEED2,
+    STOP_SPEED2,
+    
+    RUN_SPEED3,
+    STOP_SPEED3,
+
+    RUN_SPEED4,
+    STOP_SPEED4,
 };
+
+#define SPEED1_RPM   6000
+#define SPEED2_RPM   8000
+#define SPEED3_RPM   12000
+#define SPEED4_RPM   16000
--- steppy/steppy.ino
+++ steppy/steppy.ino
@@ -1,24 +1,28 @@
+/**
+ * @ Author: Elvis Chino-Islas
+ * @ Create Time: 2023-01-27 23:07:07
+ * @ Modified by: Elvis Chino-Islas
+ * @ Modified time: 2023-01-28 15:05:53
+ * @ Description:
+ */
+
 #include "steppy.h"
-#include "a4988.h"
 
-
-A4988 hw(PIN2, PIN3, PIN4);
-
+A4988 hw;
+STEP_STATES_T motor_state;
 void setup()
 {
     Serial.begin(BAUD_RATE);
-    pinMode(PIN2, OUTPUT);
-    pinMode(PIN3, OUTPUT);
-    pinMode(PIN4, OUTPUT);
     pinMode(INPUT1, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(INPUT1), handle_input1, FALLING);
 
     pinMode(INPUT2, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(INPUT2), handle_input2, FALLING);
 
-    hw.set_speed(8000);
-    hw.set_direction(true);
-    hw.start();
+    hw = A4988(ENABLE, STEP, DIR);
+
+    hw.init(8000, true);
+    motor_state = STOP_INIT;
 }
 
 volatile bool input1_intr = false;
@@ -37,33 +41,89 @@ void update_state(STEP_STATES_T &state)
 {
     switch (state)
     {
-    case STOPPED:
-        Serial.println("STOPPED");
+    case STOP_INIT:
         if (input1_intr)
         {
+            state = RUN_INIT;
             input1_intr = false;
-            Serial.println("\tINPUT1");
         }
         else if (input2_intr)
         {
             input2_intr = false;
-            Serial.println("\tINPUT2");
         }
         else
             state = state;
         break;
 
     case RUN_INIT:
-        Serial.println("RUNNING");
         if (input1_intr)
         {
+            state = RUN_SPEED1;
             input1_intr = false;
-            Serial.println("\tINPUT1");
         }
         else if (input2_intr)
         {
+            state = STOP_INIT;
             input2_intr = false;
-            Serial.println("\tINPUT2");
+        }
+        else
+            state = state;
+        break;
+        
+    case RUN_SPEED1:
+        if (input1_intr)
+        {
+            state = RUN_SPEED2;
+            input1_intr = false;
+        }
+        else if (input2_intr)
+        {
+            state = STOP_SPEED1;
+            input2_intr = false;
+        }
+        else
+            state = state;
+        break;
+
+    case RUN_SPEED2:
+        if (input1_intr)
+        {
+            state = RUN_SPEED3;
+            input1_intr = false;
+        }
+        else if (input2_intr)
+        {
+            state = STOP_SPEED2;
+            input2_intr = false;
+        }
+        else
+            state = state;
+        break;
+
+    case RUN_SPEED3:
+        if (input1_intr)
+        {
+            state = RUN_SPEED4;
+            input1_intr = false;
+        }
+        else if (input2_intr)
+        {
+            state = STOP_SPEED3;
+            input2_intr = false;
+        }
+        else
+            state = state;
+        break;
+        
+    case RUN_SPEED4:
+        if (input1_intr)
+        {
+            input1_intr = false;
+        }
+        else if (input2_intr)
+        {
+            state = STOP_SPEED3;
+            input2_intr = false;
         }
         else
             state = state;
@@ -71,13 +131,59 @@ void update_state(STEP_STATES_T &state)
     }
 }
 
-STEP_STATES_T motor_state = STOPPED;
+inline void rpm_start(A4988 driver, uint32_t rpm)
+{
+    driver.set_speed(rpm);
+    driver.stop();
+}
 
+void state_action(STEP_STATES_T state)
+{
+    switch (state)
+    {
+    case STOP_INIT:
+        Serial.println("STOPPED");
+        hw.stop();
+        break;
+    case RUN_INIT:
+        Serial.println("INIT");
+        hw.start();
+        break;
+
+    case RUN_SPEED1:
+        rpm_start(hw, SPEED1_RPM);
+        break;
+    case STOP_SPEED1:
+        hw.stop();
+        break;
+
+    case RUN_SPEED2:
+        rpm_start(hw, SPEED2_RPM);
+        break;
+    case STOP_SPEED2:
+        hw.stop();
+        break;
+
+    case RUN_SPEED3:
+        rpm_start(hw, SPEED3_RPM);
+        break;
+    case STOP_SPEED3:
+        hw.stop();
+        break;
+
+    case RUN_SPEED4:
+        rpm_start(hw, SPEED4_RPM);
+        break;
+    case STOP_SPEED4:
+        hw.stop();
+        break;
+    }
+}
 
 void loop()
 {
-    hw.sstep();
-    //update_state(motor_state);
+    update_state(motor_state);
+    state_action(motor_state);
 
-//    delay(2000);
+    hw.sstep();
 }
