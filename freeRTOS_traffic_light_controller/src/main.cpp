b3346a33988e4c2bc75f79ad0a83b8b1ee6222b1
Elvis Chino-Islas <echinoislas+github@gmail.com>
2023-04-17 22:47:15 -0700

    created two task to handle northSouth and eastWest lights
    currently not working

--- freeRTOS_traffic_light_controller/src/main.cpp
+++ freeRTOS_traffic_light_controller/src/main.cpp
@@ -1,19 +1,21 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
 #include <shift_register.h>
+#include <semphr.h>
+#include <task.h>
 
 // Total number of states in the traffic light cycle
 #define TOTAL_STATES 6
 
 // Define the bit patterns for the North-South and East-West traffic light LEDs
 // connected to the shift register pins
-#define RED_NS 0x02
-#define YELLOW_NS 0x04
-#define GREEN_NS 0x08
+#define RED_NS 1
+#define YELLOW_NS 2
+#define GREEN_NS 3
 
-#define RED_EW 0x10
-#define YELLOW_EW 0x20
-#define GREEN_EW 0x40
+#define RED_EW 4
+#define YELLOW_EW 5
+#define GREEN_EW 6
 
 // Define pins for the shift register
 #define DATA_PIN 19
@@ -22,57 +24,6 @@
 
 #define NS_SENSE 2
 #define EW_SENSE 4
-
-inline void write_to_shiftregister(uint8_t val)
-{
-    digitalWrite(LATCH_PIN, LOW);
-    shiftOut(DATA_PIN, CLOCK_PIN, MSBFIRST, val);
-    digitalWrite(LATCH_PIN, HIGH);
-}
-
-// Define the output states and durations for each state in the traffic light cycle
-const uint8_t output_data[TOTAL_STATES]{
-    RED_NS | GREEN_EW,
-    RED_NS | YELLOW_EW,
-    RED_NS | RED_EW,
-    GREEN_NS | RED_EW,
-    YELLOW_NS | RED_EW,
-    RED_NS | RED_EW};
-
-uint32_t switch_time[TOTAL_STATES]{
-    5000,
-    2500,
-    500,
-    5000,
-    2500,
-    500};
-
-void traffic_controller(void *pvParameters)
-{
-    for (;;)
-    {
-        // Iterate through each state in the traffic light cycle
-        for (uint8_t i = 0; i < TOTAL_STATES; i++)
-        {
-            // Send the current state to the shift register
-            write_to_shiftregister(output_data[i]);
-
-            // Keep the current state active until its switch_time duration has passed
-            vTaskDelay(pdMS_TO_TICKS(switch_time[i]));
-        }
-    }
-}
-
-void blinky(void *pvParameters)
-{
-    for (;;)
-    {
-        digitalWrite(LED_BUILTIN, HIGH);
-        vTaskDelay(pdMS_TO_TICKS(500));
-        digitalWrite(LED_BUILTIN, LOW);
-        vTaskDelay(pdMS_TO_TICKS(500));
-    }
-}
 
 TaskHandle_t xTaskToNotify = NULL;
 
@@ -93,31 +44,151 @@ void sense_handler_task(void *pvParameters)
     while (1)
     {
         ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Wait for the notification
-        
+
         Serial.println("Car at North-South Traffic Light");
+    }
+}
+
+enum class TrafficLightState_t
+{
+    GREEN,
+    YELLOW,
+    RED
+};
+
+struct IntersectionState_t
+{
+    TrafficLightState_t northSouth;
+    TrafficLightState_t eastWest;
+};
+
+ShiftRegister lightResource;
+SemaphoreHandle_t shiftRegisterAccess;
+SemaphoreHandle_t stateMutex;
+IntersectionState_t currentState;
+
+const TickType_t xRedDuration = pdMS_TO_TICKS(2000);
+const TickType_t xYellowDuration = pdMS_TO_TICKS(2000);
+const TickType_t xGreenDuration = pdMS_TO_TICKS(5000);
+const TickType_t xErrorDuration = pdMS_TO_TICKS(1000);
+
+void northSouthLight(void *pvParameters)
+{
+    TickType_t xDelayDuration;
+    uint8_t setMask;
+    uint8_t clearMask;
+    IntersectionState_t readState;
+
+    for (;;)
+    {
+        xSemaphoreTake(stateMutex, portMAX_DELAY);
+        Serial.println("accquired state in northSourth Task");
+        readState = currentState;
+        xSemaphoreGive(stateMutex);
+
+        switch (readState.northSouth)
+        {
+        case TrafficLightState_t::GREEN:
+            readState.northSouth = TrafficLightState_t::YELLOW;
+            setMask = 1 << GREEN_NS | 0 << YELLOW_NS | 0 << RED_NS;
+            clearMask = 0 << GREEN_NS | 1 << YELLOW_NS | 1 << RED_NS;
+            xDelayDuration = xGreenDuration;
+            break;
+        case TrafficLightState_t::YELLOW:
+            readState.northSouth = TrafficLightState_t::RED;
+            setMask = 0 << GREEN_NS | 1 << YELLOW_NS | 0 << RED_NS;
+            clearMask = 1 << GREEN_NS | 0 << YELLOW_NS | 1 << RED_NS;
+            xDelayDuration = xYellowDuration;
+            break;
+
+        case TrafficLightState_t::RED:
+            readState.northSouth = TrafficLightState_t::GREEN;
+            setMask = 0 << GREEN_NS | 0 << YELLOW_NS | 1 << RED_NS;
+            clearMask = 1 << GREEN_NS | 1 << YELLOW_NS | 0 << RED_NS;
+            xDelayDuration = xRedDuration;
+            break;
+        default:
+            readState.northSouth = TrafficLightState_t::RED;
+            xDelayDuration = xErrorDuration;
+            break;
+        }
+
+        xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+        lightResource.clear_mask(clearMask);
+        lightResource.set_mask(setMask);
+        xSemaphoreGive(shiftRegisterAccess);
+
+        vTaskDelay(xDelayDuration);
+
+        xSemaphoreTake(stateMutex, portMAX_DELAY);
+        currentState = readState;
+        xSemaphoreGive(stateMutex);
+    }
+}
+
+void eastWestLight(void *pvParameters)
+{
+    IntersectionState_t readState;
+    TickType_t xDelayDuration;
+
+    uint8_t setMask, clearMask;
+
+    for (;;)
+    {
+        xSemaphoreTake(stateMutex, portMAX_DELAY);
+        Serial.println("accquired state in eastWest Task");
+        readState = currentState;
+        xSemaphoreGive(stateMutex);
+
+        if (readState.northSouth == TrafficLightState_t::YELLOW)
+        {
+            readState.eastWest = TrafficLightState_t::GREEN;
+            setMask = 1 << GREEN_EW | 0 << YELLOW_EW | 0 << RED_EW;
+            clearMask = 0 << GREEN_EW | 1 << YELLOW_EW | 1 << RED_EW;
+            xDelayDuration = xGreenDuration;
+        }
+        else
+        {
+            readState.eastWest = TrafficLightState_t::RED;
+            setMask = 0 << GREEN_EW | 0 << YELLOW_EW | 1 << RED_EW;
+            clearMask = 1 << GREEN_EW | 1 << YELLOW_EW | 0 << RED_EW;
+            xDelayDuration = xRedDuration;
+        }
+
+        xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+        lightResource.clear_mask(clearMask);
+        lightResource.set_mask(setMask);
+        xSemaphoreGive(shiftRegisterAccess);
+
+        vTaskDelay(xDelayDuration);
+
+        xSemaphoreTake(stateMutex, portMAX_DELAY);
+        currentState = readState;
+        xSemaphoreGive(stateMutex);
     }
 }
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(DATA_PIN, OUTPUT);
-    pinMode(LATCH_PIN, OUTPUT);
-    pinMode(CLOCK_PIN, OUTPUT);
-    pinMode(LED_BUILTIN, OUTPUT);
     pinMode(NS_SENSE, INPUT_PULLUP);
     pinMode(EW_SENSE, INPUT_PULLUP);
 
+    lightResource = ShiftRegister(CLOCK_PIN, LATCH_PIN, DATA_PIN);
+    shiftRegisterAccess = xSemaphoreCreateMutex();
+    stateMutex = xSemaphoreCreateMutex();
+    currentState = {TrafficLightState_t::GREEN, TrafficLightState_t::RED};
+
     attachInterrupt(digitalPinToInterrupt(NS_SENSE), ns_sense_isr, FALLING);
 
-    xTaskCreate(traffic_controller, "Traffic Light Controller", 128, NULL, 1, NULL);
-    xTaskCreate(blinky, "Blinky", 128, NULL, 1, NULL);
+    xTaskCreate(northSouthLight, "North South Light", 128, NULL, 1, NULL);
+    xTaskCreate(eastWestLight, "East West Light", 128, NULL, 1, NULL);
     xTaskCreate(sense_handler_task, "Sense Handler", 128, NULL, 2, NULL);
 
     vTaskStartScheduler();
+
+    xSemaphoreGive(shiftRegisterAccess);
+    xSemaphoreGive(stateMutex);
 }
 
-void loop()
-{
-    // put your main code here, to run repeatedly:
-}
+void loop() {}
