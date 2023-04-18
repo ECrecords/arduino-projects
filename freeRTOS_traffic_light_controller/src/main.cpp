6d43502ae5f935f3454ab521335d71ba5474606a
Elvis Chino-Islas <echinoislas+github@gmail.com>
2023-04-18 15:12:01 -0700

    both traffic light now are dependent on eachothers state

--- freeRTOS_traffic_light_controller/src/main.cpp
+++ freeRTOS_traffic_light_controller/src/main.cpp
@@ -67,7 +67,7 @@ SemaphoreHandle_t shiftRegisterAccess;
 SemaphoreHandle_t stateMutex;
 IntersectionState_t currentState;
 
-const TickType_t xRedDuration = pdMS_TO_TICKS(2000);
+const TickType_t xRedDuration = pdMS_TO_TICKS(1000);
 const TickType_t xYellowDuration = pdMS_TO_TICKS(2000);
 const TickType_t xGreenDuration = pdMS_TO_TICKS(5000);
 const TickType_t xErrorDuration = pdMS_TO_TICKS(1000);
@@ -82,35 +82,40 @@ void northSouthLight(void *pvParameters)
     for (;;)
     {
         xSemaphoreTake(stateMutex, portMAX_DELAY);
-        Serial.println("accquired state in northSourth Task");
         readState = currentState;
         xSemaphoreGive(stateMutex);
 
-        switch (readState.northSouth)
+        if (readState.northSouth == TrafficLightState_t::GREEN)
         {
-        case TrafficLightState_t::GREEN:
             readState.northSouth = TrafficLightState_t::YELLOW;
+
             setMask = 1 << GREEN_NS | 0 << YELLOW_NS | 0 << RED_NS;
             clearMask = 0 << GREEN_NS | 1 << YELLOW_NS | 1 << RED_NS;
+
             xDelayDuration = xGreenDuration;
-            break;
-        case TrafficLightState_t::YELLOW:
+        }
+
+        else if (readState.northSouth == TrafficLightState_t::YELLOW)
+        {
             readState.northSouth = TrafficLightState_t::RED;
+
             setMask = 0 << GREEN_NS | 1 << YELLOW_NS | 0 << RED_NS;
             clearMask = 1 << GREEN_NS | 0 << YELLOW_NS | 1 << RED_NS;
-            xDelayDuration = xYellowDuration;
-            break;
 
-        case TrafficLightState_t::RED:
-            readState.northSouth = TrafficLightState_t::GREEN;
+            xDelayDuration = xYellowDuration;
+        }
+
+        else if (readState.northSouth == TrafficLightState_t::RED)
+        {
+            if (readState.eastWest == TrafficLightState_t::YELLOW)
+            {
+                readState.northSouth = TrafficLightState_t::GREEN;
+            }
+
             setMask = 0 << GREEN_NS | 0 << YELLOW_NS | 1 << RED_NS;
             clearMask = 1 << GREEN_NS | 1 << YELLOW_NS | 0 << RED_NS;
+
             xDelayDuration = xRedDuration;
-            break;
-        default:
-            readState.northSouth = TrafficLightState_t::RED;
-            xDelayDuration = xErrorDuration;
-            break;
         }
 
         xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
@@ -121,50 +126,66 @@ void northSouthLight(void *pvParameters)
         vTaskDelay(xDelayDuration);
 
         xSemaphoreTake(stateMutex, portMAX_DELAY);
-        currentState = readState;
+        currentState.northSouth = readState.northSouth;
         xSemaphoreGive(stateMutex);
     }
 }
 
 void eastWestLight(void *pvParameters)
 {
-    IntersectionState_t readState;
     TickType_t xDelayDuration;
-
-    uint8_t setMask, clearMask;
+    uint8_t setMask;
+    uint8_t clearMask;
+    IntersectionState_t readState;
 
     for (;;)
     {
         xSemaphoreTake(stateMutex, portMAX_DELAY);
-        Serial.println("accquired state in eastWest Task");
         readState = currentState;
         xSemaphoreGive(stateMutex);
 
-        if (readState.northSouth == TrafficLightState_t::YELLOW)
+        if (readState.eastWest == TrafficLightState_t::GREEN)
         {
-            readState.eastWest = TrafficLightState_t::GREEN;
+            readState.eastWest = TrafficLightState_t::YELLOW;
+
             setMask = 1 << GREEN_EW | 0 << YELLOW_EW | 0 << RED_EW;
             clearMask = 0 << GREEN_EW | 1 << YELLOW_EW | 1 << RED_EW;
+
             xDelayDuration = xGreenDuration;
         }
-        else
+
+        else if (readState.eastWest == TrafficLightState_t::YELLOW)
         {
             readState.eastWest = TrafficLightState_t::RED;
+
+            setMask = 0 << GREEN_EW | 1 << YELLOW_EW | 0 << RED_EW;
+            clearMask = 1 << GREEN_EW | 0 << YELLOW_EW | 1 << RED_EW;
+
+            xDelayDuration = xYellowDuration;
+        }
+
+        else if (readState.eastWest == TrafficLightState_t::RED)
+        {
+            if(readState.northSouth == TrafficLightState_t::YELLOW)
+            {
+                readState.eastWest = TrafficLightState_t::GREEN;
+            }
             setMask = 0 << GREEN_EW | 0 << YELLOW_EW | 1 << RED_EW;
             clearMask = 1 << GREEN_EW | 1 << YELLOW_EW | 0 << RED_EW;
+
             xDelayDuration = xRedDuration;
         }
 
-        xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
-        lightResource.clear_mask(clearMask);
-        lightResource.set_mask(setMask);
-        xSemaphoreGive(shiftRegisterAccess);
+            xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+            lightResource.clear_mask(clearMask);
+            lightResource.set_mask(setMask);
+            xSemaphoreGive(shiftRegisterAccess);
 
-        vTaskDelay(xDelayDuration);
+            vTaskDelay(xDelayDuration);
 
-        xSemaphoreTake(stateMutex, portMAX_DELAY);
-        currentState = readState;
-        xSemaphoreGive(stateMutex);
+            xSemaphoreTake(stateMutex, portMAX_DELAY);
+            currentState.eastWest = readState.eastWest;
+            xSemaphoreGive(stateMutex);
     }
 }
 
@@ -177,12 +198,14 @@ void setup()
     lightResource = ShiftRegister(CLOCK_PIN, LATCH_PIN, DATA_PIN);
     shiftRegisterAccess = xSemaphoreCreateMutex();
     stateMutex = xSemaphoreCreateMutex();
-    currentState = {TrafficLightState_t::GREEN, TrafficLightState_t::RED};
+    currentState.northSouth = TrafficLightState_t::GREEN;
+    currentState.eastWest = TrafficLightState_t::RED;
 
     attachInterrupt(digitalPinToInterrupt(NS_SENSE), ns_sense_isr, FALLING);
 
     xTaskCreate(northSouthLight, "North South Light", 128, NULL, 1, NULL);
     xTaskCreate(eastWestLight, "East West Light", 128, NULL, 1, NULL);
+
     xTaskCreate(sense_handler_task, "Sense Handler", 128, NULL, 2, NULL);
 
     vTaskStartScheduler();
