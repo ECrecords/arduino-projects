de971e6c3338a2ec8d5b16c472144db4f6b2ca17
Elvis Chino-Islas <echinoislas+github@gmail.com>
2023-04-19 19:44:29 -0700

    large changes to main file
    implmented senors that would give priority to certain traffic light, giving the North-South light
    the greatest prioirty if both are set.
    
    seperated the control into two task where are both independent on a global state.
    
    two mutex are used one for the shift register the other for the global states.

--- freeRTOS_traffic_light_controller/src/main.cpp
+++ freeRTOS_traffic_light_controller/src/main.cpp
@@ -23,31 +23,7 @@
 #define CLOCK_PIN 17
 
 #define NS_SENSE 2
-#define EW_SENSE 4
-
-TaskHandle_t xTaskToNotify = NULL;
-
-void ns_sense_isr()
-{
-    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    vTaskNotifyGiveFromISR(xTaskToNotify, &xHigherPriorityTaskWoken);
-    if (xHigherPriorityTaskWoken)
-    {
-        portYIELD_FROM_ISR();
-    }
-}
-
-void sense_handler_task(void *pvParameters)
-{
-    xTaskToNotify = xTaskGetCurrentTaskHandle();
-
-    while (1)
-    {
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Wait for the notification
-
-        Serial.println("Car at North-South Traffic Light");
-    }
-}
+#define EW_SENSE 3
 
 enum class TrafficLightState_t
 {
@@ -60,6 +36,8 @@ struct IntersectionState_t
 {
     TrafficLightState_t northSouth;
     TrafficLightState_t eastWest;
+    bool northSouthPriority;
+    bool easeWestPriority;
 };
 
 ShiftRegister lightResource;
@@ -67,125 +45,289 @@ SemaphoreHandle_t shiftRegisterAccess;
 SemaphoreHandle_t stateMutex;
 IntersectionState_t currentState;
 
-const TickType_t xRedDuration = pdMS_TO_TICKS(1000);
+const TickType_t xRedDuration = pdMS_TO_TICKS(5000);
 const TickType_t xYellowDuration = pdMS_TO_TICKS(2000);
 const TickType_t xGreenDuration = pdMS_TO_TICKS(5000);
+const TickType_t xRedGreenDuration = pdMS_TO_TICKS(3000);
 const TickType_t xErrorDuration = pdMS_TO_TICKS(1000);
+
+TaskHandle_t xNorthSouthTaskToNotify = NULL;
+TaskHandle_t xEastWestTaskToNotify = NULL;
+TickType_t lastNorthSouthInterruptTickCount = 0;
+TickType_t lastEastWestInterruptTickCount = 0;
+
+// North-South sensor interrupt service routine (ISR) with debouncing
+void NorthSouthSensorISR()
+{
+    // Debounce time interval in ticks
+    const TickType_t debounceTicks = pdMS_TO_TICKS(50);
+
+    // Get the current tick count
+    TickType_t currentTickCount = xTaskGetTickCountFromISR();
+
+    // Check if the time elapsed since the last interrupt is greater than the debounce time
+    if (currentTickCount - lastNorthSouthInterruptTickCount >= debounceTicks)
+    {
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+        vTaskNotifyGiveFromISR(xNorthSouthTaskToNotify, &xHigherPriorityTaskWoken);
+        if (xHigherPriorityTaskWoken)
+        {
+            portYIELD_FROM_ISR();
+        }
+    }
+
+    // Update the last interrupt tick count
+    lastNorthSouthInterruptTickCount = currentTickCount;
+}
+
+// East-West sensor interrupt service routine (ISR) with debouncing
+void EastWestSensorISR()
+{
+    // Debounce time interval in ticks
+    const TickType_t debounceTicks = pdMS_TO_TICKS(50);
+
+    // Get the current tick count
+    TickType_t currentTickCount = xTaskGetTickCountFromISR();
+
+    // Check if the time elapsed since the last interrupt is greater than the debounce time
+    if (currentTickCount - lastEastWestInterruptTickCount >= debounceTicks)
+    {
+        Serial.println("East-West Sensor tiggered");
+        BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+        vTaskNotifyGiveFromISR(xEastWestTaskToNotify, &xHigherPriorityTaskWoken);
+        if (xHigherPriorityTaskWoken)
+        {
+            portYIELD_FROM_ISR();
+        }
+    }
+
+    // Update the last interrupt tick count
+    lastEastWestInterruptTickCount = currentTickCount;
+}
+
+void toggleNorthSouthPriority(void *pvParameters)
+{
+    xNorthSouthTaskToNotify = xTaskGetCurrentTaskHandle();
+
+    while (1)
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Wait for the notification
+
+        Serial.println("Toggled North-South Priority");
+        xSemaphoreTake(stateMutex, portMAX_DELAY);
+        currentState.northSouthPriority = !currentState.northSouthPriority;
+        xSemaphoreGive(stateMutex);
+    }
+}
+
+void toggleEastWestPriority(void *pvParameters)
+{
+    xEastWestTaskToNotify = xTaskGetCurrentTaskHandle();
+
+    while (1)
+    {
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Wait for the notification
+        Serial.println("Toggled East-West Priority");
+        xSemaphoreTake(stateMutex, portMAX_DELAY);
+        currentState.easeWestPriority = !currentState.easeWestPriority;
+        xSemaphoreGive(stateMutex);
+    }
+}
 
 void northSouthLight(void *pvParameters)
 {
-    TickType_t xDelayDuration;
-    uint8_t setMask;
-    uint8_t clearMask;
+    TickType_t xDelayDuration = pdMS_TO_TICKS(0);
+    TickType_t xLastUpdatedTime = xTaskGetTickCount();
     IntersectionState_t readState;
+
+    uint8_t onLights;
+    uint8_t offLights;
+
+    TickType_t xCurrentTickCount;
 
     for (;;)
     {
-        xSemaphoreTake(stateMutex, portMAX_DELAY);
-        readState = currentState;
-        xSemaphoreGive(stateMutex);
-
-        if (readState.northSouth == TrafficLightState_t::GREEN)
+        if (xSemaphoreTake(stateMutex, portMAX_DELAY) == pdTRUE)
         {
-            readState.northSouth = TrafficLightState_t::YELLOW;
-
-            setMask = 1 << GREEN_NS | 0 << YELLOW_NS | 0 << RED_NS;
-            clearMask = 0 << GREEN_NS | 1 << YELLOW_NS | 1 << RED_NS;
-
-            xDelayDuration = xGreenDuration;
+            readState = currentState;
+            xSemaphoreGive(stateMutex);
         }
+        else
+            continue;
 
-        else if (readState.northSouth == TrafficLightState_t::YELLOW)
+        xCurrentTickCount = xTaskGetTickCount();
+
+        if (((xCurrentTickCount - xLastUpdatedTime) >= xDelayDuration) && !readState.northSouthPriority && !readState.easeWestPriority)
         {
-            readState.northSouth = TrafficLightState_t::RED;
 
-            setMask = 0 << GREEN_NS | 1 << YELLOW_NS | 0 << RED_NS;
-            clearMask = 1 << GREEN_NS | 0 << YELLOW_NS | 1 << RED_NS;
-
-            xDelayDuration = xYellowDuration;
-        }
-
-        else if (readState.northSouth == TrafficLightState_t::RED)
-        {
-            if (readState.eastWest == TrafficLightState_t::YELLOW)
+            switch (readState.northSouth)
             {
-                readState.northSouth = TrafficLightState_t::GREEN;
+            case TrafficLightState_t::GREEN:
+                xDelayDuration = xGreenDuration;
+                onLights = 1 << GREEN_NS;
+                offLights = 1 << RED_NS | 1 << YELLOW_NS;
+                readState.northSouth = TrafficLightState_t::YELLOW;
+                xLastUpdatedTime = xCurrentTickCount;
+                break;
+            case TrafficLightState_t::YELLOW:
+                xDelayDuration = xYellowDuration;
+                onLights = 1 << YELLOW_NS;
+                offLights = 1 << RED_NS | 1 << GREEN_NS;
+                readState.northSouth = TrafficLightState_t::RED;
+                xLastUpdatedTime = xCurrentTickCount;
+                break;
+            case TrafficLightState_t::RED:
+                if (readState.eastWest == TrafficLightState_t::RED)
+                {
+                    readState.northSouth = TrafficLightState_t::GREEN;
+                    xLastUpdatedTime = xCurrentTickCount;
+                    xDelayDuration = xRedGreenDuration;
+                }
+                onLights = 1 << RED_NS;
+                offLights = 1 << GREEN_NS | 1 << YELLOW_NS;
+
+                break;
             }
 
-            setMask = 0 << GREEN_NS | 0 << YELLOW_NS | 1 << RED_NS;
-            clearMask = 1 << GREEN_NS | 1 << YELLOW_NS | 0 << RED_NS;
+            xSemaphoreTake(stateMutex, portMAX_DELAY);
+            currentState.northSouth = readState.northSouth;
+            xSemaphoreGive(stateMutex);
 
-            xDelayDuration = xRedDuration;
+            xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+            lightResource.clear_mask(offLights);
+            lightResource.set_mask(onLights);
+            xSemaphoreGive(shiftRegisterAccess);
         }
 
-        xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
-        lightResource.clear_mask(clearMask);
-        lightResource.set_mask(setMask);
-        xSemaphoreGive(shiftRegisterAccess);
+        else if (readState.northSouthPriority)
+        {
+            readState.northSouth = TrafficLightState_t::GREEN;
+            onLights = 1 << GREEN_NS;
+            offLights = 1 << RED_NS | 1 << YELLOW_NS;
 
-        vTaskDelay(xDelayDuration);
+            xSemaphoreTake(stateMutex, portMAX_DELAY);
+            currentState.northSouth = readState.northSouth;
+            xSemaphoreGive(stateMutex);
 
-        xSemaphoreTake(stateMutex, portMAX_DELAY);
-        currentState.northSouth = readState.northSouth;
-        xSemaphoreGive(stateMutex);
+            xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+            lightResource.clear_mask(offLights);
+            lightResource.set_mask(onLights);
+            xSemaphoreGive(shiftRegisterAccess);
+        }
+
+        else if (readState.easeWestPriority)
+        {
+            readState.northSouth = TrafficLightState_t::RED;
+            onLights = 1 << RED_NS;
+            offLights = 1 << GREEN_NS | 1 << YELLOW_NS;
+
+            xSemaphoreTake(stateMutex, portMAX_DELAY);
+            currentState.northSouth = readState.northSouth;
+            xSemaphoreGive(stateMutex);
+
+            xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+            lightResource.clear_mask(offLights);
+            lightResource.set_mask(onLights);
+            xSemaphoreGive(shiftRegisterAccess);
+        }
     }
 }
 
 void eastWestLight(void *pvParameters)
 {
-    TickType_t xDelayDuration;
-    uint8_t setMask;
-    uint8_t clearMask;
+    TickType_t xDelayDuration = pdMS_TO_TICKS(0);
+    TickType_t xLastUpdatedTime = xTaskGetTickCount();
     IntersectionState_t readState;
+
+    uint8_t onLights;
+    uint8_t offLights;
+
+    TickType_t xCurrentTickCount;
 
     for (;;)
     {
-        xSemaphoreTake(stateMutex, portMAX_DELAY);
-        readState = currentState;
-        xSemaphoreGive(stateMutex);
-
-        if (readState.eastWest == TrafficLightState_t::GREEN)
+        if (xSemaphoreTake(stateMutex, portMAX_DELAY) == pdTRUE)
         {
-            readState.eastWest = TrafficLightState_t::YELLOW;
-
-            setMask = 1 << GREEN_EW | 0 << YELLOW_EW | 0 << RED_EW;
-            clearMask = 0 << GREEN_EW | 1 << YELLOW_EW | 1 << RED_EW;
-
-            xDelayDuration = xGreenDuration;
+            readState = currentState;
+            xSemaphoreGive(stateMutex);
         }
+        else
+            continue;
 
-        else if (readState.eastWest == TrafficLightState_t::YELLOW)
+        xCurrentTickCount = xTaskGetTickCount();
+
+        if (((xCurrentTickCount - xLastUpdatedTime) >= xDelayDuration) && !readState.northSouthPriority && !readState.easeWestPriority)
         {
-            readState.eastWest = TrafficLightState_t::RED;
-
-            setMask = 0 << GREEN_EW | 1 << YELLOW_EW | 0 << RED_EW;
-            clearMask = 1 << GREEN_EW | 0 << YELLOW_EW | 1 << RED_EW;
-
-            xDelayDuration = xYellowDuration;
-        }
-
-        else if (readState.eastWest == TrafficLightState_t::RED)
-        {
-            if(readState.northSouth == TrafficLightState_t::YELLOW)
+            switch (readState.eastWest)
             {
-                readState.eastWest = TrafficLightState_t::GREEN;
+            case TrafficLightState_t::GREEN:
+                xDelayDuration = xGreenDuration;
+                onLights = 1 << GREEN_EW;
+                offLights = 1 << RED_EW | 1 << YELLOW_EW;
+                readState.eastWest = TrafficLightState_t::YELLOW;
+                xLastUpdatedTime = xCurrentTickCount;
+                break;
+            case TrafficLightState_t::YELLOW:
+                xDelayDuration = xYellowDuration;
+                onLights = 1 << YELLOW_EW;
+                offLights = 1 << RED_EW | 1 << GREEN_EW;
+                readState.eastWest = TrafficLightState_t::RED;
+                xLastUpdatedTime = xCurrentTickCount;
+                break;
+            case TrafficLightState_t::RED:
+                if (readState.northSouth == TrafficLightState_t::RED)
+                {
+                    readState.eastWest = TrafficLightState_t::GREEN;
+                    xLastUpdatedTime = xCurrentTickCount;
+                    xDelayDuration = xRedGreenDuration;
+                }
+
+                onLights = 1 << RED_EW;
+                offLights = 1 << GREEN_EW | 1 << YELLOW_EW;
+
+                break;
             }
-            setMask = 0 << GREEN_EW | 0 << YELLOW_EW | 1 << RED_EW;
-            clearMask = 1 << GREEN_EW | 1 << YELLOW_EW | 0 << RED_EW;
-
-            xDelayDuration = xRedDuration;
-        }
-
-            xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
-            lightResource.clear_mask(clearMask);
-            lightResource.set_mask(setMask);
-            xSemaphoreGive(shiftRegisterAccess);
-
-            vTaskDelay(xDelayDuration);
 
             xSemaphoreTake(stateMutex, portMAX_DELAY);
             currentState.eastWest = readState.eastWest;
             xSemaphoreGive(stateMutex);
+
+            xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+            lightResource.clear_mask(offLights);
+            lightResource.set_mask(onLights);
+            xSemaphoreGive(shiftRegisterAccess);
+        }
+
+        else if (readState.northSouthPriority)
+        {
+            readState.eastWest = TrafficLightState_t::RED;
+            onLights = 1 << RED_EW;
+            offLights = 1 << GREEN_EW | 1 << YELLOW_EW;
+
+            xSemaphoreTake(stateMutex, portMAX_DELAY);
+            currentState.eastWest = readState.eastWest;
+            xSemaphoreGive(stateMutex);
+
+            xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+            lightResource.clear_mask(offLights);
+            lightResource.set_mask(onLights);
+            xSemaphoreGive(shiftRegisterAccess);
+        }
+        else if (readState.easeWestPriority)
+        {
+            readState.eastWest = TrafficLightState_t::GREEN;
+            onLights = 1 << GREEN_EW;
+            offLights = 1 << RED_EW | 1 << YELLOW_EW;
+
+            xSemaphoreTake(stateMutex, portMAX_DELAY);
+            currentState.eastWest = readState.eastWest;
+            xSemaphoreGive(stateMutex);
+
+            xSemaphoreTake(shiftRegisterAccess, portMAX_DELAY);
+            lightResource.clear_mask(offLights);
+            lightResource.set_mask(onLights);
+            xSemaphoreGive(shiftRegisterAccess);
+        }
     }
 }
 
@@ -201,12 +343,14 @@ void setup()
     currentState.northSouth = TrafficLightState_t::GREEN;
     currentState.eastWest = TrafficLightState_t::RED;
 
-    attachInterrupt(digitalPinToInterrupt(NS_SENSE), ns_sense_isr, FALLING);
+    attachInterrupt(digitalPinToInterrupt(NS_SENSE), NorthSouthSensorISR, FALLING);
+    attachInterrupt(digitalPinToInterrupt(EW_SENSE), EastWestSensorISR, FALLING);
 
     xTaskCreate(northSouthLight, "North South Light", 128, NULL, 1, NULL);
     xTaskCreate(eastWestLight, "East West Light", 128, NULL, 1, NULL);
 
-    xTaskCreate(sense_handler_task, "Sense Handler", 128, NULL, 2, NULL);
+    xTaskCreate(toggleNorthSouthPriority, "North South Sense Handler", 128, NULL, 2, NULL);
+    xTaskCreate(toggleEastWestPriority, "East West Sense Handler", 128, NULL, 2, NULL);
 
     vTaskStartScheduler();
 
